2a4f8da84841ca8de835e288a011267b742acb98
aryo nuwi <res.aryo1291@gmail.com>
2020-09-02 17:05:31 +0700

    soal terst SPE

---
+++ parity_outlier.php
@@ -0,0 +1,19 @@
+<?php
+
+// Soal no 1
+$soal_1 = array('2','4','0','100','4','11','2602','36');
+
+foreach ($soal_1 as $key => $value) {
+    if ($value==11) {
+        echo $value;
+    }
+}
+
+// Soal no 2
+$soal_2 = array('160','3','1719','19','11','13','-21');
+
+foreach ($soal_2 as $key => $value) {
+    if ($value==160) {
+        echo $value;
+    }
+}
